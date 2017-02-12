d4920fff3cdade2f2422a49c887979e74bff542d
pl461516 <ligenzapatryk2@o2.pl>
2017-02-12 16:16:40 +0100

    Add computing polygon area

--- polygon.py
+++ polygon.py
@@ -1,9 +1,24 @@
 import sys
 
-def get_area(points):
-    return points
+def get_area(vertices):
+    area = 0.0
+    for i in range(len(vertices)):
+        j = (i + 1) % len(vertices)
+        area += abs(vertices[i][0] * vertices[j][1] - vertices[j][0] * vertices[i][1])
+    return area / 2.0
+
+def get_list_of_points(coordinates):
+    vertices = []
+    for i in range(0, len(coordinates), 2):
+        vertices.append((coordinates[i], coordinates[i + 1]))
+    return vertices
 
 if __name__ == "__main__":
-    print("Test\n")
-    print(get_area(sys.argv))
-
+    if len(sys.argv[1:]) % 2 == 1:
+        print("Parameters number error!")
+    else:
+        # coordinates = [2, 4, 3, 7, 6, 10, 7, 15, 132, 34]
+        float_coordinates = [float(n) for n in sys.argv[1:]]
+        vertices = get_list_of_points(float_coordinates)
+        print(get_area(vertices))
+        # print(get_area(sys.argv[1:]))
