900508fc18255be223c6479d7e53d348facd1e15
pl461516 <ligenzapatryk2@o2.pl>
2017-02-12 15:24:32 +0100

    Test if Travis build works

--- polygon.py
+++ polygon.py
@@ -1,8 +1,9 @@
 import sys
 
 def get_area(points):
-    return points;
+    return points
 
 if __name__ == "__main__":
-    print(get_area(sys.argv[0:]))
+    print("Test\n")
+    print(get_area(sys.argv))
 
